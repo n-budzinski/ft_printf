90cd4f7963adfe669ef7f4f64ff85596ad267e5a
Norbert Budzinski <norbudzinski@outlook.com>
2024-04-03 04:07:19 +0200

    fixed the pointer handling

--- printf.c
+++ printf.c
@@ -58,6 +58,18 @@ typedef struct s_memblock
 	size_t				size;
 }	t_memblock;
 
+static char	*join_and_free(char *s1, char *s2)
+{
+	char	*ptr;
+
+	ptr = ft_strjoin(s1, s2);
+	// free(s1);
+	// free(s2);
+	if (ptr == NULL)
+		return (NULL);
+	return (ptr);
+}
+
 void	*ft_memjoin(void *m1, size_t m1_size, void *m2, size_t m2_size)
 {
 	void	*ptr;
@@ -67,11 +79,13 @@ void	*ft_memjoin(void *m1, size_t m1_size, void *m2, size_t m2_size)
 	{
 		ft_memcpy(ptr, m1, m1_size);
 		ft_memcpy(ptr + m1_size, m2, m2_size);
+		free(m1);
+		free(m2);
 	}
 	return (ptr);
 }
 
-char    *ft_create_padding(size_t n, int c)
+char    *ft_pad(size_t n, int c)
 {
 	char		*str;
 
@@ -86,13 +100,28 @@ char    *ft_create_padding(size_t n, int c)
 
 char	*ft_strndup(const char *s, size_t n)
 {
-	void	*ptr;
+	char	*ptr;
 
 	ptr = (char *)malloc(n + 1);
 	if (ptr == NULL)
 		return (NULL);
 	ft_strlcpy(ptr, s, n + 1);
 	return (ptr);
+}
+
+void	ft_lst_memblock_prepend(t_list **lst, void *mem, size_t size)
+{
+	t_list		*node;
+	t_memblock	*memblock;
+
+	memblock = malloc(sizeof(t_memblock));
+	if (memblock == NULL)
+		return ;
+	memblock->data = mem;
+	memblock->size = size;
+	node = ft_lstnew(memblock);
+	if (node)
+		ft_lstadd_front(lst, node);
 }
 
 void	ft_lst_memblock_append(t_list **lst, void *mem, size_t size)
@@ -236,6 +265,7 @@ char	*ft_ulltohexa(unsigned long long value, char *set)
 			str[len - 1 - (shift / 4)] = set[value >> shift & 0x0F];
 		}
 	}
+	str[len] = '\0';
 	return (str);
 }
 
@@ -248,46 +278,68 @@ static void	handle_chr(t_fields *fields, int c, t_list **lst)
 		return ;
 	*ptr = c;
 	if (fields->width && !fields->flg_minus)
-		ft_lst_memblock_append(lst, ft_create_padding(fields->width - 1, ' '), 1);
+		ft_lst_memblock_append(lst, ft_pad(fields->width - 1, ' '), 1);
 	ft_lst_memblock_append(lst, ptr, 1);
 	if (fields->width && fields->flg_minus)
-		ft_lst_memblock_append(lst, ft_create_padding(fields->width - 1, ' '), 1);
+		ft_lst_memblock_append(lst, ft_pad(fields->width - 1, ' '), 1);
 }
 
 static void	handle_str(t_fields *fields, char *str, t_list **lst)
 {
-	void				*ptr;
-	unsigned long long	nmem;
+	char				*ptr;
 	unsigned long long	len;
 
 	if (str == NULL)
 		return ;
 	len = ft_strlen(str);
 	if (fields->precision && fields->precision < len)
-		nmem = fields->precision;
+		len = fields->precision;
+	if (fields->width > len)
+	{
+		ptr = ft_pad(fields->width, ' ');
+		if (fields->flg_minus)
+			ft_memcpy(ptr, str, len);
+		else
+			ft_memcpy(ptr + fields->width - len, str, len);
+	}
 	else
-		nmem = len;
-	ptr = (void *)malloc(nmem);
-	if (str == NULL)
-		return ;
-	ft_memcpy(ptr, str, nmem);
-	if (fields->width && !fields->flg_minus)
-		ft_lst_memblock_append(lst, ft_create_padding(fields->width - len, ' '), fields->width - len);
-	ft_lst_memblock_append(lst, ptr, nmem);
-	if (fields->width && fields->flg_minus)
-		ft_lst_memblock_append(lst, ft_create_padding(fields->width - len, ' '), fields->width - len);
+	{
+
+		ptr = ft_strndup(str, len);
+		fields->width = len;
+	}
+	ft_lst_memblock_append(lst, ptr, fields->width);
 }
 
+
+
  static void handle_ptr(t_fields *fields, unsigned long long value, t_list **lst)
-// {
-// 	if (!value)
-// 		ft_lststr_append(lst, ft_strdup("(nil)"));
-// 	else
-// 	{
-// 		ft_lststr_append(lst, ft_strdup("0x"));
-// 		ft_lststr_append(lst, ft_ulltohexa(value, "0123456789abcdef"));
-// 	}
-// }
+{
+	unsigned long long	len;
+	char				*ptr;
+	char				*str;
+
+	str = ft_ulltohexa(value, "0123456789abcdef");
+	len = ft_strlen(str);
+	if (fields->precision > len)
+		str = join_and_free(ft_pad(fields->precision, '0'), str);
+	if (fields->width > len)
+	{
+		ptr = ft_pad(fields->width, ' ');
+		if (fields->flg_minus)
+			ft_memcpy(ptr, str, len);
+		else
+			ft_memcpy(ptr + fields->width - len, str, len);
+	}
+	else
+	{
+		ptr = ft_strndup(str, len);
+		fields->width = len;
+	}
+	if (fields->precision && fields->precision > len)
+		len = fields->precision;
+	ft_lst_memblock_append(lst, ptr, fields->width);
+}
 
 // static void	handle_dec(t_fields *fields, int value, t_list **lst)
 // {
@@ -311,19 +363,50 @@ static void	handle_str(t_fields *fields, char *str, t_list **lst)
 // 	uitoa(value, lst);
 // }
 
-// static void	handle_lhex(t_fields *fields, unsigned int value, t_list **lst)
-// {
-// 	if (fields->flg_alt)
-// 		pad_and_append(fields, ft_strdup("0x"), lst);
-// 	pad_and_append(lst, ft_ulltohexa(value, "0123456789abcdef"));
-// }
+static void apply_padding(void **str, size_t len, size_t width, char flg_minus)
+{
+	void	*ptr;
 
-// static void	handle_uhex(t_fields *fields, unsigned int value, t_list **lst)
-// {
-// 	if (fields->flg_alt)
-// 		ft_lststr_append(lst, ft_strdup("0x"));
-// 	ft_lststr_append(lst, ft_ulltohexa(value, "0123456789ABCDEF"));
-// }
+	ptr = ft_pad(width, ' ');
+	if (flg_minus)
+		ft_memcpy(ptr, *str, len);
+	else
+		ft_memcpy(ptr + width - len, *str, len);
+	free(*str);
+	*str = ptr;
+}
+
+static void	handle_hex(t_fields *fields, t_list **lst, char *str)
+{
+	unsigned long long	len;
+	void				*ptr;
+
+	len = ft_strlen(str);
+	if (fields->precision > len)
+	{
+		ptr = ft_pad(fields->precision - len, '0');
+		str = ft_memjoin(ptr, fields->precision - len, str, len);
+		len = len * 2 - fields->precision;
+	}
+	if (fields->flg_alt)
+	{
+		str = ft_memjoin(ft_strdup("0x"), 2, str, ft_strlen(str));
+		len += 2;
+	}
+	if (fields->width > len)
+		apply_padding((void **)(&str), len, fields->width, fields->flg_minus);
+	ft_lst_memblock_append(lst, str, fields->width);
+}
+
+static void	handle_lhex(t_fields *fields, unsigned int value, t_list **lst)
+{
+	handle_hex(fields, lst, ft_ulltohexa(value, "0123456789abcdef"));
+}
+
+static void	handle_uhex(t_fields *fields, unsigned int value, t_list **lst)
+{
+	handle_hex(fields, lst, ft_ulltohexa(value, "0123456789ABCDEF"));
+}
 
 static void	handle_esc(t_list **lst)
 {
@@ -342,16 +425,16 @@ void	fmt_parse(char **fstring, t_list **lst, va_list ap)
 		handle_chr(fields, va_arg(ap, int), lst);
 	if (**fstring == 's')
 		handle_str(fields, va_arg(ap, char*), lst);
-	 else if (**fstring == 'p')
-	 	handle_ptr(fields, va_arg(ap, unsigned long long), lst);
+	else if (**fstring == 'p')
+		handle_ptr(fields, va_arg(ap, unsigned long long), lst);
 	// else if (**fstring == 'd' || **fstring == 'i')
 	// 	handle_dec(fields, va_arg(ap, int), lst);
 	// else if (**fstring == 'u')
 	// 	handle_uint(fields, va_arg(ap, unsigned int), lst);
-	// else if (**fstring == 'x')
-	// 	handle_lhex(fields, va_arg(ap, unsigned int), lst);
-	// else if (**fstring == 'X')
-	// 	handle_uhex(fields, va_arg(ap, unsigned int), lst);
+	else if (**fstring == 'x')
+		handle_lhex(fields, va_arg(ap, unsigned int), lst);
+	else if (**fstring == 'X')
+		handle_uhex(fields, va_arg(ap, unsigned int), lst);
 	else if	(**fstring == '%')
 		handle_esc(lst);
 	free(fields);
@@ -411,9 +494,7 @@ int	main()
 	char *word2 = "CRUEL";
 	char c = '!';
 	unsigned int uint = 94121;
-	ft_printf("HELLO %c %c %10.3s\n", 0, '0', word);
-	//printf("HELLO %c %10.3s\n", 0, word);
-	//ft_printf("HELLO, %s %s WORLD%c %#X%p\n", word, word2, c, uint, NULL);
-	//printf("HELLO, %s %s WORLD%c %#X%p\n", word, word2, c, uint, NULL);
+	ft_printf("HELLO %#25x\n", uint);
+	printf("HELLO %#25x\n", uint);
 	return (0);
 }
